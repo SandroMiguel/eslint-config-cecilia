24961859cdfbd97037f07234f14dfb40b33916e6
Sandro Miguel <sandromiguel@sandromiguel.com>
2020-10-17 14:04:42 +0100

    test(test spec): replace CLIEngine with ESLint class

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,109 +1,39 @@
-/* eslint-disable no-console */
-const { ESLint } = require('eslint');
+const { ESLint } = require('eslint')
 
-(async function run() {
-  // 1. Create an instance.
+const fetchTestFiles = async rule => {
   const eslint = new ESLint({ ignore: false })
+  const results = await eslint.lintFiles([`test/fixtures/**/${rule}.js`])
+  return results
+}
 
-  // 2. Lint files.
-  const basePath = 'test/fixtures/'
-  const results = await eslint.lintFiles([`${basePath}**/*.js`])
-  // return results
+expect.extend({
+  toBe(received, expected, message) {
+    const pass = received === expected
+    if (pass) {
+      return {
+        message: () => `expected '${expected}' | received '${received}'`,
+        pass: true,
+      }
+    }
+    return {
+      message: () =>
+        `${this.utils.matcherHint('toBe', received, expected)}\n` +
+        `This test case expected violation of rule ${this.utils.printExpected(
+          expected,
+        )} but received ${this.utils.printReceived(
+          received,
+        )} | message: ${this.utils.printReceived(message)}`,
+      pass: false,
+    }
+  },
+})
 
-  // await ESLint.outputFixes(results)
-
-  // // 4. Format the results.
-  // const formatter = await eslint.loadFormatter('stylish')
-  // const resultText = formatter.format(results)
-
-  // return resultText
-
-  results.forEach(({ filePath, messages }) => {
-    console.log('file ->', filePath)
-    const folder = filePath.substring(
-      filePath.indexOf(basePath) + basePath.length,
-      filePath.lastIndexOf('/') + 1,
-    )
-    const filename = filePath.substring(filePath.lastIndexOf('/') + 1)
-    const ruleId = filename.substring(0, filename.lastIndexOf('.'))
-
-    describe(filename, () => {
-      test('detect the correct error ID', () => {
-        console.log('-->', ruleId, messages)
-        expect(messages[0].ruleId).toStrictEqual(`${folder}${ruleId}`)
-        console.log(
-          `${filename} - ${messages[0].ruleId} | ${messages[0].message}`,
-        )
-      })
-    })
-  })
-})()
-
-// const results = run().then(res => {
-//   // console.log('res ->', res)
-//   return res
-// })
-
-// console.log('r:', results)
-
-// results.forEach(({ filePath, messages }) => {
-//   console.log('file ->', filePath)
-//   const basePath = 'test/fixtures/'
-
-//   const folder = filePath.substring(
-//     filePath.indexOf(basePath) + basePath.length,
-//     filePath.lastIndexOf('/') + 1,
-//   )
-//   const filename = filePath.substring(filePath.lastIndexOf('/') + 1)
-//   const ruleId = filename.substring(0, filename.lastIndexOf('.'))
-
-//   describe(filename, () => {
-//     test('detect the correct error ID', () => {
-//       console.log('-->', ruleId, messages)
-//       expect(messages[0].ruleId).toStrictEqual(`${folder}${ruleId}`)
-//       console.log(
-//         `${filename} - ${messages[0].ruleId} | ${messages[0].message}`,
-//       )
-//     })
-//   })
-// })
-
-// (async function main() {
-//   // 1. Create an instance.
-//   const eslint = new ESLint({ ignore: false })
-
-//   // 2. Lint files.
-//   const basePath = 'test/fixtures/'
-//   const results = await eslint.lintFiles([`${basePath}**/*.js`])
-//   //console.log('results:', results)
-
-//   results.forEach(({ filePath, messages }) => {
-//     console.log('file ->', filePath)
-//     const folder = filePath.substring(
-//       filePath.indexOf(basePath) + basePath.length,
-//       filePath.lastIndexOf('/') + 1,
-//     )
-//     const filename = filePath.substring(filePath.lastIndexOf('/') + 1)
-//     const ruleId = filename.substring(0, filename.lastIndexOf('.'))
-
-//     describe(filename, () => {
-//       test('detect the correct error ID', () => {
-//         console.log('-->', ruleId, messages)
-//         expect(messages[0].ruleId).toStrictEqual(`${folder}${ruleId}`)
-//         console.log(
-//           `${filename} - ${messages[0].ruleId} | ${messages[0].message}`,
-//         )
-//       })
-//     })
-//   })
-
-//   // 3. Format the results.
-//   const formatter = await eslint.loadFormatter('stylish')
-//   const resultText = formatter.format(results)
-
-//   // 4. Output it.
-//   console.log(resultText)
-// })().catch(error => {
-//   process.exitCode = 1
-//   console.error(error)
-// });
+test('camelcase rule', async () => {
+  const ruleId = 'camelcase'
+  const result = await fetchTestFiles(ruleId)
+  const { messages } = result[0]
+  expect(messages[0] && messages[0].ruleId).toBe(
+    ruleId,
+    messages[0] && messages[0].message,
+  )
+})
