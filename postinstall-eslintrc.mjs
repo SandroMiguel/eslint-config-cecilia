bbe081c3f6e133594b1e1b73c685de9894483d7b
Sandro <sandromiguel@sandromiguel.com>
2023-01-17 16:55:23 +0000

    feat: create the .eslintrc.js automatically

---
+++ postinstall-eslintrc.mjs
@@ -0,0 +1,24 @@
+import readline from 'node:readline'
+import { exec } from 'node:child_process'
+import fs from 'node:fs'
+
+const readLineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+readLineInterface.question(
+  'Do you wish to create the ".eslintrc.js" file automatically? (y/n) ',
+  (userInput) => {
+    readLineInterface.close()
+    if (userInput.toLowerCase() === 'y') {
+      fs.writeFile('.eslintrc.js', '{"extends": "cecilia"}', (error) => {
+        if (error) {
+          console.log(error)
+          return
+        }
+        console.log('The file was created!')
+      })
+    }
+  },
+)
--- postinstall-lint.mjs
+++ postinstall-lint.mjs
@@ -5,15 +5,14 @@ const readLineInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
-// const readLineInterface = readline.createInterface({ input, output })
 
 readLineInterface.question(
   'Do you wish to add the "lint" script command in your package.json? (y/n) ',
   (userInput) => {
     readLineInterface.close()
     if (userInput.toLowerCase() === 'y') {
-      console.log('Now you can use "npm run lint" to lint your files.')
       exec('npm pkg set scripts.lint="eslint ."')
+      console.log('Now you can use "npm run lint" to lint your files.')
     } else {
       console.log(
         'You can manually add this command in your package.json scripts: "lint": "eslint ."',
@@ -21,18 +20,3 @@ readLineInterface.question(
     }
   },
 )
-
-// const userInput = await readLineInterface.question(
-//   'Do you wish to add the "lint" script command in your package.json (y/n)? ',
-// )
-
-// if (userInput.toLowerCase() === 'y') {
-//   console.log('Now you can use "npm run lint" to lint your files.')
-//   exec('npm pkg set scripts.lint="eslint ."')
-// } else {
-//   console.log(
-//     'You can manually add this command in your package.json scripts: "lint": "eslint ."',
-//   )
-// }
-
-// readLineInterface.close()
